1810710adad933ebef8b2eb5cd5a87e5bc0ecb17
fueripe-desu <fueripedesu2020@outlook.com>
2025-01-13 21:20:46 -0300

    test(loader): improve util functions tests redability

--- internal/loader/util_test.go
+++ internal/loader/util_test.go
@@ -6,248 +6,207 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
-	"time"
 
 	"github.com/fueripe-desu/gofidential/errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func Test_isValidFolder(t *testing.T) {
-	t.Run("valid folder", func(t *testing.T) {
-		// Arrange
+func Test_isValidDir(t *testing.T) {
+	t.Run("should return true if the path points to a valid dir", func(t *testing.T) {
 		assert := assert.New(t)
-		tempName := "test"
+		require := require.New(t)
 
-		// Act
-		tempDir, err := os.MkdirTemp("", tempName)
+		tempDirName := "test"
 
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
+		tempDir, err := os.MkdirTemp("", tempDirName)
+		require.NoError(err, "Failed to create temporary dir")
 
-		defer os.RemoveAll(tempDir)
+		t.Cleanup(func() {
+			err = os.RemoveAll(tempDir)
+			assert.NoError(err, "Failed to remove temporary dir")
+		})
 
-		err = isValidFolder(tempDir)
+		err = isValidDir(tempDir)
+		require.NoError(err, "An unexpected error ocurred")
 
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
-
-		// Assert
-		assert.Nil(err)
+		require.Nil(err)
 	})
 
-	t.Run("folder not found", func(t *testing.T) {
-		// Arrange
-		assert := assert.New(t)
+	t.Run("should return an error if the specified folder was not found", func(t *testing.T) {
+		require := require.New(t)
+
 		expectedErr := newEnvFolderNotExistError()
 
-		// Act
-		err := isValidFolder("unknown")
-
-		// Assert
-		if err == nil {
-			assert.FailNow("Error should not be nil.")
-		}
+		err := isValidDir("unknown")
+		require.Error(err, "An error was expected. But got none.")
 
 		castErr, ok := err.(*errors.GofidentialError)
+		require.True(ok, "Error is not of type GofidentialError.")
 
-		if !ok {
-			assert.FailNow("Error is not of type GofidentialError.")
-		}
-
-		assert.Equal(castErr.Issuer, expectedErr.Issuer)
-		assert.Equal(castErr.Code, expectedErr.Code)
-		assert.Equal(castErr.Message, expectedErr.Message)
-		assert.WithinDuration(castErr.Timestamp, expectedErr.Timestamp, 5*time.Second)
-		assert.Equal(castErr.Suggestion, expectedErr.Suggestion)
-		assert.NotEmpty(castErr.StackTrace, expectedErr.StackTrace)
-		assert.Equal(castErr.Details, expectedErr.Details)
+		require.True(
+			castErr.Equal(expectedErr),
+			"The actual error does not match the expected one. Actual: %v, Expected: %v",
+			castErr,
+			expectedErr,
+		)
 	})
 
-	t.Run("filepath", func(t *testing.T) {
-		// Arrange
+	t.Run("should return an error if the specified path does not point to a dir", func(t *testing.T) {
+		require := require.New(t)
 		assert := assert.New(t)
-		tempName := "test"
+
+		tempDirName := "test"
 		tempFilename := "example.txt"
+
 		expectedErr := newPathIsNotFolderError()
 
-		// Act
-		tempDir, err := os.MkdirTemp("", tempName)
-
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
-
-		defer os.RemoveAll(tempDir)
+		tempDir, err := os.MkdirTemp("", tempDirName)
+		require.NoError(err, "Failed to create temporary dir")
 
 		fp := filepath.Join(tempDir, tempFilename)
+
 		tempFile, err := os.OpenFile(fp, os.O_CREATE|os.O_WRONLY, 0644)
+		require.NoError(err, "Failed to create file in temporary dir.")
 
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
-		defer tempFile.Close()
+		t.Cleanup(func() {
+			err := tempFile.Close()
+			assert.NoError(err, "Failed to close file in temporary dir.")
 
-		err = isValidFolder(fp)
+			err = os.RemoveAll(tempDir)
+			assert.NoError(err, "Failed to remove temporary dir")
+		})
 
-		// Assert
-		if err == nil {
-			assert.FailNow("Error should not be nil.")
-		}
+		err = isValidDir(fp)
+		require.Error(err, "An error was expected. But got none.")
 
 		castErr, ok := err.(*errors.GofidentialError)
+		require.True(ok, "Error is not of type GofidentialError.")
 
-		if !ok {
-			assert.FailNow("Error is not of type GofidentialError.")
-		}
-
-		assert.Equal(castErr.Issuer, expectedErr.Issuer)
-		assert.Equal(castErr.Code, expectedErr.Code)
-		assert.Equal(castErr.Message, expectedErr.Message)
-		assert.WithinDuration(castErr.Timestamp, expectedErr.Timestamp, 5*time.Second)
-		assert.Equal(castErr.Suggestion, expectedErr.Suggestion)
-		assert.NotEmpty(castErr.StackTrace, expectedErr.StackTrace)
-		assert.Equal(castErr.Details, expectedErr.Details)
+		require.True(
+			castErr.Equal(expectedErr),
+			"The actual error does not match the expected one. Actual: %v, Expected: %v",
+			castErr,
+			expectedErr,
+		)
 	})
 }
 
 func Test_fileExists(t *testing.T) {
-	t.Run("file exists", func(t *testing.T) {
-		// Arrange
+	t.Run("should return true if the file exists", func(t *testing.T) {
+		require := require.New(t)
 		assert := assert.New(t)
-		tempName := "test"
+
+		tempDirName := "test"
 		tempFilename := "example.txt"
 
-		// Act
-		tempDir, err := os.MkdirTemp("", tempName)
-
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
-
-		defer os.RemoveAll(tempDir)
+		tempDir, err := os.MkdirTemp("", tempDirName)
+		require.NoError(err, "Failed to create temporary dir")
 
 		fp := filepath.Join(tempDir, tempFilename)
-		tempFile, err := os.OpenFile(fp, os.O_CREATE|os.O_WRONLY, 0644)
 
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
-		defer tempFile.Close()
+		tempFile, err := os.OpenFile(fp, os.O_CREATE|os.O_WRONLY, 0644)
+		require.NoError(err, "Failed to create file in temporary dir.")
+
+		t.Cleanup(func() {
+			err := tempFile.Close()
+			assert.NoError(err, "Failed to close file in temporary dir.")
+
+			err = os.RemoveAll(tempDir)
+			assert.NoError(err, "Failed to remove temporary dir")
+		})
 
 		exists, err := fileExists(fp)
+		require.NoError(err, "An unexpected error ocurred")
 
-		if err != nil {
-			assert.FailNow("Error should not be nil.")
-		}
-
-		assert.True(exists)
-		assert.Nil(err)
+		require.True(exists)
+		require.Nil(err)
 	})
 
-	t.Run("inexistent file", func(t *testing.T) {
-		// Arrange
-		assert := assert.New(t)
-		tempFilename := "unknown.txt."
+	t.Run("should return false if the file does not exist", func(t *testing.T) {
+		require := require.New(t)
+		tempFilename := "unknown.txt"
 
-		// Act
 		exists, err := fileExists(tempFilename)
+		require.NoError(err, "An unexpected error ocurred")
 
-		// Assert
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
-
-		assert.False(exists)
-		assert.Nil(err)
+		require.False(exists)
+		require.Nil(err)
 	})
 
-	t.Run("inexistent file", func(t *testing.T) {
-		// Arrange
+	t.Run("should return an error if the specified path does not point to a file", func(t *testing.T) {
+		require := require.New(t)
 		assert := assert.New(t)
+
 		tempName := "test"
+
 		expectedErr := newPathIsNotFileError()
 
-		// Act
 		tempDir, err := os.MkdirTemp("", tempName)
+		require.NoError(err, "Failed to create temporary dir")
 
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
-
-		defer os.RemoveAll(tempDir)
+		t.Cleanup(func() {
+			err := os.RemoveAll(tempDir)
+			assert.NoError(err, "Failed to remove temporary dir")
+		})
 
 		exists, err := fileExists(tempDir)
-
-		// Assert
-		if err == nil {
-			assert.FailNow("Error should not be nil.")
-		}
+		require.Error(err, "An error was expected. But got none.")
 
 		castErr, ok := err.(*errors.GofidentialError)
+		require.True(ok, "Error is not of type GofidentialError.")
 
-		if !ok {
-			assert.FailNow("Error is not of type GofidentialError.")
-		}
-
-		assert.False(exists)
-		assert.Equal(castErr.Issuer, expectedErr.Issuer)
-		assert.Equal(castErr.Code, expectedErr.Code)
-		assert.Equal(castErr.Message, expectedErr.Message)
-		assert.WithinDuration(castErr.Timestamp, expectedErr.Timestamp, 5*time.Second)
-		assert.Equal(castErr.Suggestion, expectedErr.Suggestion)
-		assert.NotEmpty(castErr.StackTrace, expectedErr.StackTrace)
-		assert.Equal(castErr.Details, expectedErr.Details)
+		require.False(exists)
+		require.True(
+			castErr.Equal(expectedErr),
+			"The actual error does not match the expected one. Actual: %v, Expected: %v",
+			castErr,
+			expectedErr,
+		)
 	})
 }
 
 func Test_openFile(t *testing.T) {
-	t.Run("valid file", func(t *testing.T) {
-		// Arrange
+	t.Run("should return a valid byte buffer if file exists", func(t *testing.T) {
+		require := require.New(t)
 		assert := assert.New(t)
+
 		tempName := "test"
 		tempFilename := "example.txt"
+
 		fileContent := "Hello world!"
 
-		// Act
 		tempDir, err := os.MkdirTemp("", tempName)
-
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
-
-		defer os.RemoveAll(tempDir)
+		require.NoError(err, "Failed to create temporary dir")
 
 		fp := filepath.Join(tempDir, tempFilename)
+
 		tempFile, err := os.OpenFile(fp, os.O_CREATE|os.O_WRONLY, 0644)
+		require.NoError(err, "Failed to create file in temporary dir.")
 
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
+		t.Cleanup(func() {
+			err = os.RemoveAll(tempDir)
+			assert.NoError(err, "Failed to remove temporary dir")
+		})
 
-		defer tempFile.Close()
+		_, err = tempFile.WriteString(fileContent)
+		require.NoError(err, "Failed writing string to file.")
 
-		if _, err = tempFile.WriteString(fileContent); err != nil {
-			assert.FailNow(err.Error())
-		}
+		err = tempFile.Close()
+		assert.NoError(err, "Failed to close file in temporary dir.")
 
 		actualBuffer, err := openFile(fp)
-
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
+		require.NoError(err, "An unexpected error ocurred")
 
 		var expectedBuffer bytes.Buffer
+
 		file, err := os.Open(fp)
-
-		if err != nil {
-			assert.FailNow(err.Error())
-		}
-
-		defer file.Close()
+		require.NoError(err, "Could not open file to fill expected buffer.")
 
 		_, err = io.Copy(&expectedBuffer, file)
+
+		err = file.Close()
+		assert.NoError(err, "Could not close expected buffer file.")
 
 		assert.Equal(actualBuffer.String(), expectedBuffer.String())
 		assert.NotEmpty(actualBuffer.String())
@@ -255,34 +214,28 @@ func Test_openFile(t *testing.T) {
 		assert.Nil(err)
 	})
 
-	t.Run("inexistent file", func(t *testing.T) {
-		// Arrange
+	t.Run("should return an error if the specified file path does not exist", func(t *testing.T) {
+		require := require.New(t)
 		assert := assert.New(t)
+
 		filename := "unknown.txt"
+
 		expectedErr := newFailedToReadFileError()
 
-		// Act
 		buffer, err := openFile(filename)
-
-		// Assert
-		if err == nil {
-			assert.FailNow("Error should not be nil.")
-		}
+		require.Error(err, "An error was expected. But got none.")
 
 		castErr, ok := err.(*errors.GofidentialError)
-
-		if !ok {
-			assert.FailNow("Error is not of type GofidentialError.")
-		}
+		require.True(ok, "Error is not of type GofidentialError.")
 
 		assert.Empty(buffer.String())
 		assert.Equal(buffer.Len(), 0)
-		assert.Equal(castErr.Issuer, expectedErr.Issuer)
-		assert.Equal(castErr.Code, expectedErr.Code)
-		assert.Equal(castErr.Message, expectedErr.Message)
-		assert.WithinDuration(castErr.Timestamp, expectedErr.Timestamp, 5*time.Second)
-		assert.Equal(castErr.Suggestion, expectedErr.Suggestion)
-		assert.NotEmpty(castErr.StackTrace, expectedErr.StackTrace)
-		assert.Equal(castErr.Details, expectedErr.Details)
+		require.Error(err, "An error was expected. But got none.")
+		require.True(
+			castErr.Equal(expectedErr),
+			"The actual error does not match the expected one. Actual: %v, Expected: %v",
+			castErr,
+			expectedErr,
+		)
 	})
 }
