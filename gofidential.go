d217fde9c6d7ff003ba4aef80de7ef438762afaa
fueripe-desu <fueripedesu2020@outlook.com>
2025-01-18 20:47:26 -0300

    feat: implement main Load() function

--- gofidential.go
+++ gofidential.go
@@ -1,6 +1,29 @@
 package gofidential
 
-func Load(data any) error {
+import (
+	loader "github.com/fueripe-desu/gofidential/internal/loader"
+	parser "github.com/fueripe-desu/gofidential/internal/parser"
+	reflector "github.com/fueripe-desu/gofidential/internal/reflector"
+)
 
-	return nil
+type Environment struct {
+	Name           string
+	OverridePath   string
+	IgnoreFilename bool
+}
+
+func Load(s any, env Environment) error {
+	byteData, err := loader.Load(env.Name, env.OverridePath, env.IgnoreFilename)
+
+	if err != nil {
+		return err
+	}
+
+	parsedData, err := parser.Parse(byteData)
+
+	if err != nil {
+		return err
+	}
+
+	return reflector.Reflect(parsedData, s)
 }
