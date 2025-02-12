962a7bd43517e34855de6b7d5df74c5e29c39e0f
fueripe-desu <fueripedesu2020@outlook.com>
2025-02-12 02:09:17 -0300

    chore(examples): add load-from-env example

---
+++ examples/load-from-env/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	gf "github.com/fueripe-desu/gofidential"
+)
+
+type Secrets struct {
+	AppName string
+	Version int
+}
+
+func main() {
+	os.Setenv("APP_NAME", "Gofidential")
+	os.Setenv("VERSION", "1")
+
+	env := gf.Environment{Name: "dev", LoadFromEnv: true}
+	s := Secrets{}
+
+	err := gf.Load(&s, env)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Printf("App name: '%v'\n", s.AppName)
+	fmt.Printf("Version: '%v'\n", s.Version)
+
+	os.Unsetenv("APP_NAME")
+	os.Unsetenv("VERSION")
+}
